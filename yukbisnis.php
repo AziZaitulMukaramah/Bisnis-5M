4586d672b15bda9f6e6a33eda3899b224c41e711
AziZaitulMukaramah <159240082+AziZaitulMukaramah@users.noreply.github.com>
2024-03-06 15:28:18 +0700

    Create yukbisnis.php

---
+++ yukbisnis.php
@@ -0,0 +1,12 @@
+<html>
+  <head>
+    <title>Mari Berbisnis Bersamaku</title>
+    <body>
+      <?php
+echo "Yamg penting Halal";
+echo "Gajinya gedek";
+echo "Bersama Azi Zaitul Mukaramah";
+<?
+    </body>
+  </head>
+</html>
